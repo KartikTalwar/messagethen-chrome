60c4146ad66a3fe56fbdcfdc8934305083026f3b
Geoffrey Vedernikoff <yefim323@gmail.com>
2014-01-19 03:07:51 -0500

    Prettier

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,19 @@
+var DEFAULT_MINUTES = 1;
+
+// 2014-01-19T02:21:46.761Z
+Date.prototype.toDateInputValue = (function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+  console.log(local.toJSON());
+  return local.toJSON().slice(0,10);
+});
+
+Date.prototype.toTimeInputValue = (function() {
+  var local = new Date(this);
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset() + DEFAULT_MINUTES);
+  return local.toJSON().slice(11,16);
+});
+
 $(function() {
   var $textbox = $('[placeholder="Write a reply..."]');
   var $container = $textbox.parent().parent().parent().parent().parent();
@@ -6,40 +22,62 @@ $(function() {
   var sendlater = '<label class="_1ri uiButton uiButtonConfirm"><input value="Send Later" type="button" class="sendlater _5f0v" tabindex=""></label>';
   $emoticons.prepend(sendlater);
 
-
+  // open sendlater calendar or login to FB
   var sl = function(e) {
 
+    // open calendar view
     var openSendlater = function() {
-      var picker = "<input placeholder='Message when?' type='text' id='when'>";
-      $emoticons.prepend(picker);
-      
-      $('#when').intimidatetime({
-        format: 'u',
-        previewFormat: 'yyyy-MM-dd HH:mm:ss'
-      })
-      $('#when')[0].click();  // hack
+      var calendar = "<ul class='calendar'>" +
+                       "<li><input id='date' type='date'></li>" +
+                       "<li><input id='time' type='time'></li>" +
+                       "<li><strong id='when'></strong></li>" +
+                     "</ul>";
 
-      var n = window.location.toString().lastIndexOf('/');
-      var url = "https://graph.facebook.com/" + window.location.toString().substring(n + 1);
-      
+      $emoticons.prepend(calendar);
+
+      // default date to today
+      $('[type=date]').val(new Date().toDateInputValue());
+      $('[type=time]').val(new Date().toTimeInputValue());
+
+      var updateTime = function() {
+        var date = $('#date').val();
+        var time = $('#time').val();
+        
+        var day = moment(date + " " + time);
+        $('#when').text(day.calendar());
+      };
+      updateTime();
+      $('.calendar input').on('change', updateTime);
+
       var messagethen = '<label class="_1ri uiButton uiButtonConfirm"><input id="messagethen" value="Message Then" type="button" class="_5f0v" tabindex=""></label>';
       $emoticons.find('._1ri').replaceWith(messagethen);
 
       $('#messagethen').on('click', function(e) {
+        $('#messagethen').addClass('green');
+        $('#messagethen').val('Sending...');
+
+        var n = window.location.toString().lastIndexOf('/');
+        var url = "https://graph.facebook.com/" + window.location.toString().substring(n + 1);
         // get fb id of recipient
         $.getJSON(url, function(data) {
           var name = data.name;
           var message = $textbox.val();
           var accessToken = localStorage.getItem("accessToken");
-          var when = $('#when').val(); // in epochs
+          var when = "" + moment($('#date').val() + " " + $('#time').val()).unix();
+          console.log(when);
 
           var data = {"name": name, "message": message, "token": accessToken, "when": when};
 
           $.post('http://messagethen.com/post', data, function(r) {
             $textbox.val('');
-            $emoticons.find('._1ri').replaceWith(sendlater);
-            $emoticons.find('#when').remove();
-            $('.sendlater').on('click', sl);
+            $emoticons.find('.calendar').remove();
+
+            $('#messagethen').val('Scheduled!');
+            setTimeout(function() {
+              $emoticons.find('._1ri').replaceWith(sendlater);
+              $('.sendlater').on('click', sl);
+            }, 1800);
+
           });
         });
       });
